025d41bbb5c34bfa2fd80b45d1209c516d56c8e9
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-26 16:56:29 +0200

    done

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -1,12 +1,35 @@
 package com.epam.rd.autotasks.matrices;
+
 import java.util.Arrays;
 
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        int columnsOfFirstMatrix = matrix1[0].length;
+        int rowsOfSecondMatrix = matrix2.length;
 
-        // Put your code here
+        if (columnsOfFirstMatrix != rowsOfSecondMatrix) {
+            throw new IllegalArgumentException(String.format("Can't multiply matrices, columns of first matrix"
+                            + " %d is not equal to rows of second matrix %d",
+                    columnsOfFirstMatrix, rowsOfSecondMatrix));
+        }
 
-        return null;
+        int rowsOfFirstMatrix = matrix1.length;
+        int columnsOfSecondMatrix = matrix2[0].length;
+        int[][] product = new int[rowsOfFirstMatrix][columnsOfSecondMatrix];
+
+        for (int i = 0; i < rowsOfFirstMatrix; i++) {
+            for (int j = 0; j < columnsOfSecondMatrix; j++) {
+
+                int sum = 0;
+                for (int k = 0; k < rowsOfSecondMatrix; k++) {
+                    sum = sum + matrix1[i][k] * matrix2[k][j];
+                }
+
+                product[i][j] = sum;
+            }
+        }
+
+        return product;
     }
 
     public static void main(String[] args) {
@@ -18,11 +41,11 @@ public class MultiplyMatrix {
         int[][] a = {
                 {0, 12345},
                 {4509, 0},
-                {3, 567} };
+                {3, 567}};
 
         int[][] b = {
                 {653, 0, 25353},
-                {0, 61, 6} };
+                {0, 61, 6}};
 
         int[][] result = multiply(a, b);
         System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
